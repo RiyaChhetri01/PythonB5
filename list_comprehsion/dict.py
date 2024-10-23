56b7162e320fb0a1fcb4687513bc99ccdec9602f
Riya Chhetri <chhetriria1234@gmail.com>
2024-10-23 11:35:21 +0530

    list and dictionary

---
+++ list_comprehsion/dict.py
@@ -0,0 +1,5 @@
+squares_dict = {x: x**2 for x in range(5)}
+print(squares_dict)
+fruits = ['apple', 'banana', 'cherry']
+fruit_dict = {fruit: len(fruit) for fruit in fruits}
+print(fruit_dict)
---
+++ list_comprehsion/list_making.py
@@ -0,0 +1,7 @@
+squares = [x**2 for x in range(10)]
+print(squares)
+evens = [x for x in range(20) if x % 2 == 0]
+print(evens)
+fruits = ['apple', 'banana', 'cherry']
+fruit_lengths = [len(fruit) for fruit in fruits]
+print(fruit_lengths)
